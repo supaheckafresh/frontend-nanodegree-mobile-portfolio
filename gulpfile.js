6c7a6a1001893e7516b165dbb32dcfd6f8ad3fe1
supaheckafresh <neilstrain@gmail.com>
2015-12-02 18:10:49 -0800

    add package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
+//TODO add src and dist (or maybe dev / production ) folders to project. Output minified files to dist directory.
+
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     minifyCSS = require('gulp-minify-css');
+
+gulp.task('default',
+    [
+        'scripts',
+        'styles'
+    ]
+);
 
 gulp.task('scripts', function() {
     gulp.src('js/*.js')
