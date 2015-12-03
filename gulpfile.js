773a8b2a9c61a9520f006796a3fde1468fb92b05
supaheckafresh <neilstrain@gmail.com>
2015-12-02 17:36:58 -0800

    minify perfmatters.js with gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,8 @@ var gulp = require('gulp'),
     rename = require('gulp-rename');
 
 gulp.task('scripts', function() {
-    
+    gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(rename('perfmatters.min.js'))
+        .pipe(gulp.dest('js/'));
 });
