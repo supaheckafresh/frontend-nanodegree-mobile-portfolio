9b76f1114cd32041533c1e02deead3e2d8209f92
supaheckafresh <neilstrain@gmail.com>
2015-12-02 17:45:24 -0800

    gulp minify css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    minifyCSS = require('gulp-minify-css');
 
 gulp.task('scripts', function() {
     gulp.src('js/*.js')
         .pipe(uglify())
         .pipe(rename('perfmatters.min.js'))
         .pipe(gulp.dest('js/'));
+});
+
+gulp.task('styles', function() {
+    gulp.src('css/*.css')
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('minCSS/'));
 });
